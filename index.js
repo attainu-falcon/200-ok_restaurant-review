9e9a0af1af26c3095a92612eb43bb287545363e2
shahrukh2893 <shahrukhanwer92@gmail.com>
2019-06-20 11:52:54 +0530

    click through for owner landing and add restaurant

--- index.js
+++ index.js
@@ -2,9 +2,15 @@ const express = require("express");
 const app = express();
 const customerhome = require("./customerhome");
 const restaurant = require("./restaurant");
+const ownerlanding = require("./ownerlanding");
+const addrestaurant = require("./addrestaurant");
+const editrestaurant = require("./editrestaurant");
 
 app.use("/customerhome", customerhome);
 app.use("/restaurant", restaurant);
+app.use("/ownerlanding", ownerlanding);
+app.use("/addrestaurant", addrestaurant);
+app.use("/editrestaurant", editrestaurant);
 
 app.listen(3000, () => {
   console.log("listening on Port 3000");
